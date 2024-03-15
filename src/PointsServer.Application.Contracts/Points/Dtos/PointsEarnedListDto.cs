25411ec86c63225dc60697801b79ce0d6dfbca2e
suer <suer@schrodingernft.ai>
2024-03-15 12:21:01 +0800

    add action

--- src/PointsServer.Application.Contracts/Points/Dtos/PointsEarnedListDto.cs
+++ src/PointsServer.Application.Contracts/Points/Dtos/PointsEarnedListDto.cs
@@ -12,6 +12,12 @@ public class PointsEarnedListDto
     public string FirstSymbolAmount { get; set; }
     public string SecondSymbolAmount { get; set; }
     public string ThirdSymbolAmount { get; set; }
+    public string FourSymbolAmount { get; set; }
+    public string FiveSymbolAmount { get; set; }
+    public string SixSymbolAmount { get; set; } 
+    public string SevenSymbolAmount { get; set; } 
+    public string EightSymbolAmount { get; set; } 
+    public string NineSymbolAmount { get; set; } 
     public long FollowersNumber { get; set; }
     public long UpdateTime { get; set; }
     public decimal Rate { get; set; }
