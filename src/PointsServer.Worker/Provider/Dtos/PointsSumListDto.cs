dc90982bd3dce6cba4ff6c72387ba8abc9279b96
suer <suer@schrodingernft.ai>
2024-03-15 20:49:49 +0800

    fix

--- src/PointsServer.Worker/Provider/Dtos/PointsSumListDto.cs
+++ src/PointsServer.Worker/Provider/Dtos/PointsSumListDto.cs
@@ -25,6 +25,13 @@ public class PointsSumDto
     public long FirstSymbolAmount { get; set; }
     public long SecondSymbolAmount { get; set; }
     public long ThirdSymbolAmount { get; set; }
+    public long FourSymbolAmount { get; set; }
+    public long FiveSymbolAmount { get; set; }
+    public long SixSymbolAmount { get; set; } 
+    public long SevenSymbolAmount { get; set; } 
+    public long EightSymbolAmount { get; set; } 
+    public long NineSymbolAmount { get; set; } 
+    
     public DateTime CreateTime { get; set; }
     public DateTime UpdateTime { get; set; }
 }
